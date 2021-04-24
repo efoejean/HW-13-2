a87484b4009f5aa797fea459130033778c353cd8
efoejean <efoejean@gmail.com>
2021-04-24 14:32:28 -0500

    QuerySelect the random button and add random fctn from MDN

--- scripts.js
+++ scripts.js
@@ -4,6 +4,13 @@ const root = document.querySelector(':root');
 const minFormNum = document.querySelector('#min-num');
 const maxFormNum = document.querySelector('#max-num');
 const color = document.querySelector('input[type="color"]');
+const btn = document.querySelector('.button--random');
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
 
 color.addEventListener('input', event => {
   root.style.setProperty('--color', `${event.target.value}`);
