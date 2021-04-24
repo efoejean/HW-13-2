4271d9c188f21f7fcd017ec96382e9ffe1f05469
efoejean <efoejean@gmail.com>
2021-04-24 13:59:39 -0500

    Const Root, Const Color, Listen to input event to change color of the root.

--- scripts.js
+++ scripts.js
@@ -1,8 +1,13 @@
 const p = document.createElement('p');
 document.body.appendChild(p);
+const root = document.querySelector(':root');
 const minFormNum = document.querySelector('#min-num');
 const maxFormNum = document.querySelector('#max-num');
+const color = document.querySelector('input[type="color"]');
 
+color.addEventListener('input', event => {
+  root.style.setProperty('--color', `${event.target.value}`);
+});
 minFormNum.addEventListener('submit', event => {
   event.preventDefault();
   const newMin = event.target.elements[0].value;
