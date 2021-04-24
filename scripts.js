48cb5f972ae5e2595607b8e0676b66d273a9773d
efoejean <efoejean@gmail.com>
2021-04-24 13:25:55 -0500

    Submit the  Min and Max form. log them to make sure they work

--- scripts.js
+++ scripts.js
@@ -1,2 +1,14 @@
 const minFormNum = document.querySelector('#min-num');
 const maxFormNum = document.querySelector('#max-num');
+
+minFormNum.addEventListener('submit', event => {
+  event.preventDefault();
+  const newMin = event.target.elements[0].value;
+  console.log(newMin);
+});
+
+maxFormNum.addEventListener('submit', event => {
+  event.preventDefault();
+  const newMax = event.target.elements[0].value;
+  console.log(newMax);
+});
